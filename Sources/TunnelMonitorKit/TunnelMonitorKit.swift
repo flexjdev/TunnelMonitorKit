fb86c21fad88ef0489c9b23a5b60d7d962d9ae45
Chris Janusiewicz <chris.janusiewicz@coderus.com>
2022-03-19 10:21:34 +0000

    chore: add license
    Add MIT License file and Xcode macro for custom file headers.

--- Sources/TunnelMonitorKit/TunnelMonitorKit.swift
+++ Sources/TunnelMonitorKit/TunnelMonitorKit.swift
@@ -1,3 +1,11 @@
+//
+//  TunnelMonitorKit.swift
+//  TunnelMonitorKit
+//
+//  Created by Chris J on 19/03/2022.
+//  Copyright © 2022 Chris Janusiewicz. Distributed under the MIT License.
+//
+
 public struct TunnelMonitorKit {
     public private(set) var text = "Hello, World!"
 
