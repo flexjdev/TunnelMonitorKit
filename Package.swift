8df3a9cbe1e9eb592cd31ef03dae25db86311310
Chris Janusiewicz <kpjanusiewicz@gmail.com>
2022-04-17 23:21:11 +0100

    ci: constrain macos version to 10.12
    Constrain macos version to enable building and testing using swift build

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "TunnelMonitorKit",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v10),
+        .macOS(.v10_12)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
